9af95b457641df27aeb2f79dbd230b47a3e91e1e
Ian Bautista <victor.jackal@gmail.com>
2019-06-16 22:55:01 +0800

    Conversion to  typescript phase 3

--- src/schemas.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function buildSchemas(db) {
-    var createRideTableSchema = "\n        CREATE TABLE Rides\n        (\n        rideID INTEGER PRIMARY KEY AUTOINCREMENT,\n        startLat DECIMAL NOT NULL,\n        startLong DECIMAL NOT NULL,\n        endLat DECIMAL NOT NULL,\n        endLong DECIMAL NOT NULL,\n        riderName TEXT NOT NULL,\n        driverName TEXT NOT NULL,\n        driverVehicle TEXT NOT NULL,\n        created DATETIME default CURRENT_TIMESTAMP\n        )\n    ";
-    db.run(createRideTableSchema);
-    return db;
-}
-exports.buildSchemas = buildSchemas;
