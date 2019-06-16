ba07dbc582d05dd682e5d234fe53884d9bb5b4b8
Ian Bautista <victor.jackal@gmail.com>
2019-06-16 23:35:53 +0800

    Conversion to typescript, add nyc/mocha

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,14 +1,16 @@
 import request from "supertest";
 
+import { expressApp } from "../src/app";
+import { buildSchemas } from "../src/schemas";
+
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(":memory:");
 
-const app = require("../src/app")(db);
-import { buildSchemas } from "../src/schemas";
-
+// const app = require("../src/app")(db);
+const thisApp = expressApp(db);
 describe("API tests", () => {
   before(done => {
-    db.serialize(err => {
+    db.serialize((err: Error) => {
       if (err) {
         return done(err);
       }
@@ -21,7 +23,7 @@ describe("API tests", () => {
 
   describe("GET /health", () => {
     it("should return health", done => {
-      request(app)
+      request(thisApp)
         .get("/health")
         .expect("Content-Type", /text/)
         .expect(200, done);
